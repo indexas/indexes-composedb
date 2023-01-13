ece89f67e4d0d986d8298439be7372bf4e7924ff
serafettin <mail@serafettin.net>
2023-01-13 18:11:52 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css'
 
 import {CeramicWrapper} from "../context";
 import type { AppProps } from 'next/app'
+import CeramicProvider from "../context/CeramicProvider";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <CeramicWrapper>
-      <Component {...pageProps} ceramic />
+        <CeramicProvider>
+            <Component {...pageProps} ceramic />
+        </CeramicProvider>
     </CeramicWrapper>
   );
 }
